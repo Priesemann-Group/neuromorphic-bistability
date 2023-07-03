ceb16007e4495eae5c859e30b2b35110289c7212
Johannes Zierenberg <zierenberg@users.noreply.github.com>
2023-07-03 17:44:31 +0200

    Delete experiments/hardware directory
    This was an intermediate state; final state is hosted at an internal BrainScale server. Please contact ElectrionicVision(s) lab in case you are interested in the low-level code.

--- experiments/hardware/experiment/powercycle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Poweroff"
-srun -p cube --wafer 68 --fpga-without 3 --gres cubex8 -c 1 --mem 1G -t 5:00 --pty -- singularity exec --app visionary-dls /containers/stable/latest hxcube_enable_fpga.py 3 --poweroff
-
-echo "Poweron"
-srun -p cube --wafer 68 --fpga-without 3 --gres cubex8 -c 1 --mem 1G -t 5:00 --pty -- singularity exec --app visionary-dls /containers/stable/latest hxcube_enable_fpga.py 3 --skip-bootloader
-
-echo "Done"
